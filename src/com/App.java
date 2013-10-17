d740ef328dc84ef3ed1e4c4555438a5fa1f7c7c5
agstm2012 <agstm2012@ya.ru>
2013-10-17 20:18:12 +0400

    Добавил класс запускающий приложение

---
+++ src/com/App.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivan
+ * Date: 10/17/13
+ * Time: 8:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+}
